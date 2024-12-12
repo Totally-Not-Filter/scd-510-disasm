46f47f2386831cf960128a479ffceaed69dfdfa6
Filter <not.so.filter@hotmail.com>
2024-12-12 18:49:10 -0500

    make header more similar to source code

--- sorted/header/header.asm
+++ sorted/header/header.asm
@@ -1,36 +1,36 @@
-		dc.l	$00FFFFFF
-		dc.l	$FFFFFFFF
-		dc.l	$FFFFFF00
-		dc.l	$00020001
-		dc.b	"SEGADISCSYSTEM  "
-		dc.b	"SEGAIPSAMP ", $00, $00, $00, $00, $01
-		dc.b	"SYSTEM NAME", $00, $00, $00, $00, $00
-		dc.b	$00, $00, $08, $00, $00, $00, $08, $00, $00, $00, $00, $00, $00, $00, $00, $00
-		dc.b	$00, $00, $10, $00, $00, $00, $70, $00, $00, $00, $00, $00, $00, $00, $00, $00
-		dc.b	"                "
-		dc.b	"                "
-		dc.b	"                "
-		dc.b	"                "
-		dc.b	"                "
-		dc.b	"                "
-		dc.b	"                "
-		dc.b	"                "
-		dc.b	"                "
-		dc.b	"                "
-		dc.b	"                "
-		dc.b	"SEGA MEGA DRIVE "
-		dc.b	"(C)SEGA 1993.APR"
-		dc.b	"CD-SONIC THE HEDGEHOG                           "
-		dc.b	"CD-SONIC THE HEDGEHOG                           "
-		dc.b	"XX 00000000-00"
-		dc.w	$2020
-		dc.b	"J               "
-		dc.l	$20202020
-		dc.l	$20202020
-		dc.l	$20202020
-		dc.l	$20202020
-		dc.l	$20202020
-		dc.l	$20202020
-		dc.l	$20202020
-		dc.b	"                                                    "
-		dc.b	"J               "
+		DC.L	$00FFFFFF
+		DC.L	$FFFFFFFF
+		DC.L	$FFFFFF00
+		DC.L	$00020001
+		DC.B	"SEGADISCSYSTEM  "
+		DC.B	"SEGAIPSAMP ", $00, $00, $00, $00, $01
+		DC.B	"SYSTEM NAME", $00, $00, $00, $00, $00
+		DC.B	$00, $00, $08, $00, $00, $00, $08, $00, $00, $00, $00, $00, $00, $00, $00, $00
+		DC.B	$00, $00, $10, $00, $00, $00, $70, $00, $00, $00, $00, $00, $00, $00, $00, $00
+		DC.B	"                "
+		DC.B	"                "
+		DC.B	"                "
+		DC.B	"                "
+		DC.B	"                "
+		DC.B	"                "
+		DC.B	"                "
+		DC.B	"                "
+		DC.B	"                "
+		DC.B	"                "
+		DC.B	"                "
+		DC.B	"SEGA MEGA DRIVE "
+		DC.B	"(C)SEGA 1993.APR"
+		DC.B	"CD-SONIC THE HEDGEHOG                           "
+		DC.B	"CD-SONIC THE HEDGEHOG                           "
+		DC.B	"XX 00000000-00"
+		DC.W	$2020
+		DC.B	"J               "
+		DC.L	$20202020
+		DC.L	$20202020
+		DC.L	$20202020
+		DC.L	$20202020
+		DC.L	$20202020
+		DC.L	$20202020
+		DC.L	$20202020
+		DC.B	"                                                    "
+		DC.B	"J               "
